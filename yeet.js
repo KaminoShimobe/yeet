61c9455ca27ac1495a8d6c6b2e18d1b9706096c1
KaminoShimobe <AbdulKirby@gmail.com>
2019-10-25 20:51:00 -0500

    SPOTIFY BOIS

--- yeet.js
+++ yeet.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const TwitchBot = require('twitch-bot');
+const SpotifyWebApi = require('spotify-web-api-node');
 const prefix = "!";
 const mods = new Set();
 const subs = new Set();
@@ -10,6 +11,12 @@ const Bot = new TwitchBot({
   oauth: process.env.TWITCH,
   channels: ['falsevibrato8']
 })
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: '0fd564583e5c45e5be778d459dae3131',
+  clientSecret: process.env.SPOTIFY,
+  redirectUri: 'https://discord.gg/khQuZ62'
+});
  
 Bot.on('join', channel => {
   console.log(`Joined channel: ${channel}`)
@@ -24,7 +31,7 @@ Bot.on('error', err => {
  
 Bot.on('message', chatter => {
   if(chatter.message === '!help' || chatter.message.indexOf("help") != -1 || chatter.message.indexOf("Help") != -1) {
-    Bot.say('Contact one of our lovely mods for help with anything! Except Kamino he a bum :P');
+    Bot.say('Contact one of our lovely mods for help with anything!');
    
   }	
 	
@@ -32,10 +39,45 @@ Bot.on('message', chatter => {
     Bot.say('Join our ranch! We got da horses in da back! https://discord.gg/khQuZ62')
   }
 
+  if(chatter.message === '!spotify' || chatter.message.indexOf("spotify") != -1 || chatter.message.indexOf("Spotify") != -1 || chatter.message.indexOf("Spotify?") != -1 || chatter.message.indexOf("spotify?") != -1) {
+    Bot.say('Join our ranch! We got da horses in da back! https://discord.gg/khQuZ62')
+  }
+
+if(chatter.username === 'Kamino_Shimobe' || chatter.display_name === 'Kamino_Shimobe' || chatter.username === 'psytician' || chatter.display_name === 'Psytician' || chatter.username === 'falsevibrato8' || chatter.display_name === 'falsevibrato8'){
+  if(chatter.message === '!toggle'){
+		con.query(`SELECT * FROM spotify WHERE id = 'steelBarnBot'`, (err, rows) => {
+		if(err) throw err;
+		let sql;
+
+		let accept = rows[0].accept;
+
+		if(rows.length < 1) {
+			sql = `INSERT INTO user (accept) VALUES (${true})`;
+			con.query(sql, console.log);
+			Bot.say(`Accepting spotify requests!`);
+			return;
+		} else {
+			if(accept == true){
+				sql = `UPDATE spotify SET accept = ${false} WHERE id = 'steelBarnBot'`;
+				con.query(sql, console.log);
+				Bot.say(`Not accepting spotify requests!`);
+			} else if(accept == false){
+				sql = `UPDATE spotify SET accept = ${true} WHERE id = 'steelBarnBot'`;
+				con.query(sql, console.log);
+				Bot.say(`Accepting spotify requests!`);
+			}
+
+		}
+
+
+		});	
+	}
+}
+
   if(chatter.username === 'Kamino_Shimobe' || chatter.display_name === 'Kamino_Shimobe') {
 	var chance = Math.floor(Math.random() * 4) + 1;	  
 	  if(chance == 1){
-    		Bot.say('OMG SHUT UP KAMINO :( :( :(');
+    		Bot.say('Kamino kicked me out of the house...');
 	  }
   }
 	
@@ -49,42 +91,42 @@ if(chatter.username === 'psytician' || chatter.display_name === 'Psytician') {
 
 const bot = new Discord.Client({disableEveryone: true})
 
-// var con_fig = {
-// 	host: "",
-// 	user: "",
-// 	password: process.env.MY_SQL,
-// 	database: "",
-// 	port: 3306
-// };
+var con_fig = {
+	host: "us-cdbr-iron-east-02.cleardb.net",
+	user: "ba70974f187526",
+	password: process.env.MY_SQL,
+	database: "heroku_2433a99852a1991",
+	port: 3306
+};
 
-// var con;
+var con;
 
-// function handleDisconnect() {
-// con = mysql.createConnection(con_fig);
-// con.connect(function(err) {              // The server is either down
-//     if(err) {                                     // or restarting (takes a while sometimes).
-//       console.log('error when connecting to db:', err);
-//       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-//     }                                     // to avoid a hot loop, and to allow our node script to
-//   }); 	
+function handleDisconnect() {
+con = mysql.createConnection(con_fig);
+con.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  }); 	
 
-// process.on('uncaughtException', function (err) {
-//     console.log(err);
+process.on('uncaughtException', function (err) {
+    console.log(err);
 	
-// }); 
+}); 
 	
 
 
-// con.on('error', function(err) {
-//     console.log('db error', err);
-//     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-//       handleDisconnect();                         // lost due to either server restart, or a
-//     } else {                                      // connnection idle timeout (the wait_timeout
-//        throw err;                                 // server variable configures this)
-//     }
-// });
-//        }
-// handleDisconnect();
+con.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+       throw err;                                 // server variable configures this)
+    }
+});
+       }
+handleDisconnect();
 
 
  
@@ -123,11 +165,50 @@ bot.on("message", async message => {
 
 	let args = messageArray.slice(1);
 
+	if(command === `${prefix}table`){
+	if(message.author.id == '242118931769196544'){
+		var sql = "CREATE TABLE spotify (accept BOOLEAN)";
+		
+		
+		con.query(sql, function (err, result) {
+    	if (err) throw err;
+    	message.author.send("Table Spotify created!");
+  	});
 	
+		
+
 	
+}
+	}	
+	
+	if(command === `${prefix}spotify`){
+		con.query(`SELECT * FROM spotify WHERE id = 'steelBarnBot'`, (err, rows) => {
+		if(err) throw err;
+		let sql;
+
+		let accept = rows[0].accept;
+
+		if(rows.length < 1) {
+			sql = `INSERT INTO user (accept) VALUES (${true})`;
+			con.query(sql, console.log);
+			message.channel.send(`Accepting spotify requests!`)
+			return;
+		} else {
+			if(accept == true){
+				sql = `UPDATE spotify SET accept = ${false} WHERE id = 'steelBarnBot'`;
+				con.query(sql, console.log);
+				message.channel.send(`Not accepting spotify requests!`);
+			} else if(accept == false){
+				sql = `UPDATE spotify SET accept = ${true} WHERE id = 'steelBarnBot'`;
+				con.query(sql, console.log);
+				message.channel.send(`Accepting spotify requests!`);
+			}
+
+		}
 
 
-	
+		});	
+	}
 
 
 		
@@ -139,6 +220,16 @@ if(command === `${prefix}stream`){
 	message.channel.send("False be streamin here: \n https://m.twitch.tv/falsevibrato8/profile \n GO CHECK HER OUT CUZ")
 }
 	
+if(command === `${prefix}playlist`){
+	spotifyApi.getMe()
+  	.then(function(data) {
+    console.log('Some information about the authenticated user', data.body);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+	
+}
+
 
 	
 });	
